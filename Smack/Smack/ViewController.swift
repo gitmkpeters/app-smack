891e21d51bd33016ebc663308e053c3a9287c73b
mkpeters <mike.peterson@jprsoftware.com>
2018-01-12 12:48:10 -0700

    init commit

---
+++ Smack/Smack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Mike on 1/12/18.
+//  Copyright © 2018 jprsoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
